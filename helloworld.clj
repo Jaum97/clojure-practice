0a47281773f5499d8c1f44056e2d19162805eac6
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-21 23:36:17 -0300

    update

--- helloworld.clj
+++ helloworld.clj
@@ -1,3 +1,4 @@
+;;https://clojurebridge.org/community-docs/docs/getting-started/helloworld/
 user=> (prn "Hello World")
 "Hello World"
 nil
@@ -5,3 +6,9 @@ user=> (println "Hello World")
 Hello World
 nil
 user=> (pr-str "Hello World")
+
+;;https://clojure.org/about/functional_programming
+(def hello (fn [] "Hello world"))
+-> #'user/hello
+(hello)
+-> "Hello world"
