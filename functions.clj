e4ef014e04f7dc6542eebd14d80dd3a1fbbbcc81
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-08-22 23:58:19 -0300

    functions.clj

---
+++ functions.clj
@@ -0,0 +1,8 @@
+;;https://clojure.org/guides/learn/functions
+
+;;    name   params         body
+;;    -----  ------  -------------------
+(defn greet  [name]  (str "Hello, " name) )
+
+user=> (greet "students")
+;;"Hello, students"
